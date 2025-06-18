52d38e379d265c44c6e05e079c0e0044ec8f6cf2
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-06-18 13:39:17 +0200

    first Lean commit

---
+++ QuantumComputer3Qubits.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `QuantumComputer3Qubits` library.
+-- Import modules here that should be built as part of the library.
+import QuantumComputer3Qubits.Basic
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # quantum_computer_3_qubits
-Lean 4 formalization of quantum computer with 3 qubits
