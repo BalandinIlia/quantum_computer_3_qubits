46696a63d110b0f3abfa850d34328d36b78276ca
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-06-24 21:09:05 +0200

    +file

--- QuantumComputer3Qubits.lean
+++ QuantumComputer3Qubits.lean
@@ -5,6 +5,7 @@ import QuantumComputer3Qubits.QubitIndexed
 import QuantumComputer3Qubits.QubitSet
 import QuantumComputer3Qubits.TensorProduct
 import QuantumComputer3Qubits.Complex
+import QuantumComputer3Qubits.InnerProduct
 
 #check Basis.equivFun
 #check Basis.tensorProduct
