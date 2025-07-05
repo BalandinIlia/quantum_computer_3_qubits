9284f8645510958e241daafe1756f54795e39c13
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-07-05 08:30:17 +0200

    refactoring

--- QuantumComputer3Qubits/Tests/RegistryState.lean
+++ QuantumComputer3Qubits/Tests/RegistryState.lean
@@ -1,4 +1,5 @@
 import QuantumComputer3Qubits.Formalization.RegistryState
+import QuantumComputer3Qubits.Tests.StateExamples
 
 -- check that modules are defined for all registry states
 #synth Module ℂ StateReg1
@@ -13,15 +14,6 @@ import QuantumComputer3Qubits.Formalization.RegistryState
 #synth IP (StateReg1Ind 1)
 #synth IP (StateReg2Ind 0 2 (by simp))
 #synth IP StateReg3
-
-noncomputable
-def st00: StateReg2 := TensorProduct.tmul ℂ QZero QZero
-noncomputable
-def st01: StateReg2 := TensorProduct.tmul ℂ QZero QOne
-noncomputable
-def st10: StateReg2 := TensorProduct.tmul ℂ QOne QZero
-noncomputable
-def st11: StateReg2 := TensorProduct.tmul ℂ QOne QOne
 
 -- prove or-statement by proving left part
 macro "pol": tactic =>
@@ -45,18 +37,18 @@ macro "po": tactic =>
 })
 
 theorem test1:
-  (IP.f st00 st01 = 0) ∧
-  (IP.f st00 st10 = 0) ∧
-  (IP.f st00 st11 = 0) ∧
-  (IP.f st01 st00 = 0) ∧
-  (IP.f st01 st10 = 0) ∧
-  (IP.f st01 st11 = 0) ∧
-  (IP.f st10 st01 = 0) ∧
-  (IP.f st10 st00 = 0) ∧
-  (IP.f st10 st11 = 0) ∧
-  (IP.f st11 st01 = 0) ∧
-  (IP.f st11 st10 = 0) ∧
-  (IP.f st11 st00 = 0) := by
+  (IP.f SE.s00 SE.s01 = 0) ∧
+  (IP.f SE.s00 SE.s10 = 0) ∧
+  (IP.f SE.s00 SE.s11 = 0) ∧
+  (IP.f SE.s01 SE.s00 = 0) ∧
+  (IP.f SE.s01 SE.s10 = 0) ∧
+  (IP.f SE.s01 SE.s11 = 0) ∧
+  (IP.f SE.s10 SE.s01 = 0) ∧
+  (IP.f SE.s10 SE.s00 = 0) ∧
+  (IP.f SE.s10 SE.s11 = 0) ∧
+  (IP.f SE.s11 SE.s01 = 0) ∧
+  (IP.f SE.s11 SE.s10 = 0) ∧
+  (IP.f SE.s11 SE.s00 = 0) := by
   all_goals try apply And.intro
   all_goals try apply And.intro
   all_goals try apply And.intro
@@ -68,19 +60,17 @@ theorem test1:
   all_goals try apply And.intro
   all_goals try apply And.intro
   all_goals try apply And.intro
-  all_goals simp [st00, st01, st10, st11]
   all_goals simp [IP.f, IP.IPLeft, IP.IPRight]
   all_goals try po
   all_goals simp [IP.f, QZero, QOne]
 
 theorem test2:
-  (IP.f st00 st00 = 1) ∧
-  (IP.f st01 st01 = 1) ∧
-  (IP.f st10 st10 = 1) ∧
-  (IP.f st11 st11 = 1) := by
+  (IP.f SE.s00 SE.s00 = 1) ∧
+  (IP.f SE.s01 SE.s01 = 1) ∧
+  (IP.f SE.s10 SE.s10 = 1) ∧
+  (IP.f SE.s11 SE.s11 = 1) := by
   all_goals try apply And.intro
   all_goals try apply And.intro
   all_goals try apply And.intro
-  all_goals simp [st00, st01, st10, st11]
   all_goals simp [IP.f, IP.IPLeft, IP.IPRight]
   all_goals simp [QZero, QOne]
--- QuantumComputer3Qubits/Tests/StateExamples.lean
+++ QuantumComputer3Qubits/Tests/StateExamples.lean
@@ -1,1 +1,54 @@
 import QuantumComputer3Qubits.Formalization.RegistryState
+
+-- SE means "State Examples"
+namespace SE
+
+@[simp]
+def s0: StateReg1 :=
+fun x:Fin 2 => match x with
+| 0 => 1
+| 1 => 0
+@[simp]
+def s1: StateReg1 :=
+fun x:Fin 2 => match x with
+| 0 => 0
+| 1 => 1
+
+@[simp]
+noncomputable
+def s00: StateReg2 := TensorProduct.tmul ℂ QZero QZero
+@[simp]
+noncomputable
+def s01: StateReg2 := TensorProduct.tmul ℂ QZero QOne
+@[simp]
+noncomputable
+def s10: StateReg2 := TensorProduct.tmul ℂ QOne QZero
+@[simp]
+noncomputable
+def s11: StateReg2 := TensorProduct.tmul ℂ QOne QOne
+
+@[simp]
+def QZeroInd(i: Fin 3): StateReg1Ind i :=
+match i with
+| 0 => fun x: X1 => match x with
+                    | X1.a => 1
+                    | X1.b => 0
+| 1 => fun x: X2 => match x with
+                    | X2.a => 1
+                    | X2.b => 0
+| 2 => fun x: X3 => match x with
+                    | X3.a => 1
+                    | X3.b => 0
+
+@[simp]
+def QOneInd(i: Fin 3): StateReg1Ind i :=
+match i with
+| 0 => fun x: X1 => match x with
+                    | X1.a => 0
+                    | X1.b => 1
+| 1 => fun x: X2 => match x with
+                    | X2.a => 0
+                    | X2.b => 1
+| 2 => fun x: X3 => match x with
+                    | X3.a => 0
+                    | X3.b => 1
