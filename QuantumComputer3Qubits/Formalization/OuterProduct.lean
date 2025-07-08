717d1e46cb3a7f8911fadc2fc8853d45241365e9
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-07-08 07:49:39 +0200

    OP

--- QuantumComputer3Qubits/Formalization/OuterProduct.lean
+++ QuantumComputer3Qubits/Formalization/OuterProduct.lean
@@ -4,14 +4,14 @@ def OP{T: Type}
       [AddCommMonoid T]
       [m: Module ℂ T]
       [IP T]
-      (t: T): T →ₗ[ℂ] T :=
+      (t1 t2: T): T →ₗ[ℂ] T :=
 {
-  toFun(x: T) := (IP.f t x) • t
+  toFun(x: T) := (IP.f t2 x) • t1
   map_add' := by
     simp [IP.distrRight]
     intro x y
-    generalize r₁:IP.f t x = X
-    generalize r₂:IP.f t y = Y
+    generalize r₁:IP.f t2 x = X
+    generalize r₂:IP.f t2 y = Y
     clear x y r₁ r₂
     simp [m.add_smul]
   map_smul' := by
--- QuantumComputer3Qubits/Tests/OuterProduct.lean
+++ QuantumComputer3Qubits/Tests/OuterProduct.lean
@@ -4,20 +4,20 @@ import QuantumComputer3Qubits.Tests.StateExamples
 namespace Test_OuterProduct
 
 theorem test1:
-(OP SE.s00 SE.s00 = SE.s00) ∧
-(OP SE.s00 SE.s01 = 0) ∧
-(OP SE.s00 SE.s10 = 0) ∧
-(OP SE.s00 SE.s11 = 0) ∧
-(OP SE.s01 SE.s00 = 0) ∧
-(OP SE.s01 SE.s01 = SE.s01) ∧
-(OP SE.s01 SE.s10 = 0) ∧
-(OP SE.s01 SE.s11 = 0) ∧
-(OP SE.s10 SE.s00 = 0) ∧
-(OP SE.s10 SE.s01 = 0) ∧
-(OP SE.s10 SE.s10 = SE.s10) ∧
-(OP SE.s10 SE.s11 = 0) ∧
-(OP SE.s11 SE.s00 = 0) ∧
-(OP SE.s11 SE.s01 = 0) ∧
-(OP SE.s11 SE.s10 = 0) ∧
-(OP SE.s11 SE.s11 = SE.s11) := by
+(OP SE.s00 SE.s00 SE.s00 = SE.s00) ∧
+(OP SE.s00 SE.s00 SE.s01 = 0) ∧
+(OP SE.s00 SE.s00 SE.s10 = 0) ∧
+(OP SE.s00 SE.s00 SE.s11 = 0) ∧
+(OP SE.s01 SE.s01 SE.s00 = 0) ∧
+(OP SE.s01 SE.s01 SE.s01 = SE.s01) ∧
+(OP SE.s01 SE.s01 SE.s10 = 0) ∧
+(OP SE.s01 SE.s01 SE.s11 = 0) ∧
+(OP SE.s10 SE.s10 SE.s00 = 0) ∧
+(OP SE.s10 SE.s10 SE.s01 = 0) ∧
+(OP SE.s10 SE.s10 SE.s10 = SE.s10) ∧
+(OP SE.s10 SE.s10 SE.s11 = 0) ∧
+(OP SE.s11 SE.s11 SE.s00 = 0) ∧
+(OP SE.s11 SE.s11 SE.s01 = 0) ∧
+(OP SE.s11 SE.s11 SE.s10 = 0) ∧
+(OP SE.s11 SE.s11 SE.s11 = SE.s11) := by
   simp [OP, IP.f, IP.IPLeft, IP.IPRight, IP.Transfer.lE, isoQubitIndQubitBase]
