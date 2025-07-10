82bf3595b13df6d4c7e85d0b8293e98326a988eb
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-07-10 21:23:30 +0200

    fix

--- QuantumComputer3Qubits/Tests/HermitianConjugation.lean
+++ QuantumComputer3Qubits/Tests/HermitianConjugation.lean
@@ -2,6 +2,7 @@ import QuantumComputer3Qubits.Formalization.InnerProduct
 import QuantumComputer3Qubits.Formalization.OrthonormalBasis
 import QuantumComputer3Qubits.Formalization.OuterProduct
 import QuantumComputer3Qubits.Formalization.HermitianConjugation
+import QuantumComputer3Qubits.Formalization.FiniteSum
 
 namespace Test_HermitianConjugation
 open HC
@@ -58,67 +59,18 @@ adj (A+B) = (adj A) + (adj B) := by
   apply Set.toFinite (Function.support AA)
   apply Set.toFinite (Function.support BB)
 
-private axiom ax1{T: Type}
-                 [AddCommMonoid T]
-                 [Module ℂ T]
-                 (N: ℕ)
-                 (A: Fin N → (T →ₗ[ℂ] T))
-                 (x: T):
-(finsum A) x = finsum (fun i: Fin N => A i x)
-
-private axiom ax2(T: Type)
-                 [AddCommMonoid T]
-                 (N: ℕ)
-                 (op: T → T → ℂ)
-                 (opPR1: ∀x1 x2 y: T, op (x1+x2) y = op x1 y + op x2 y)
-                 (opPR2: ∀x y1 y2: T, op x (y1+y2) = op x y1 + op x y2)
-                 (S: Fin N → T)
-                 (x: T):
-op (finsum S) x = finsum (fun i: Fin N => op (S i) x)
-
-private axiom ax3(T: Type)
-                 [AddCommMonoid T]
-                 (N: ℕ)
-                 (op: T → T → ℂ)
-                 (opPR1: ∀x1 x2 y: T, op (x1+x2) y = op x1 y + op x2 y)
-                 (opPR2: ∀x y1 y2: T, op x (y1+y2) = op x y1 + op x y2)
-                 (S: Fin N → T)
-                 (x: T):
-op x (finsum S) = finsum (fun i: Fin N => op x (S i))
-
-private axiom ax4{T: Type}
-                 [AddCommMonoid T]
-                 [Module ℂ T]
-                 (N: ℕ)
-                 (A: (T →ₗ[ℂ] T))
-                 (x: Fin N → T):
-A (finsum x) = finsum (fun i: Fin N => A (x i))
-
-private axiom ax5(T: Type)
-                 [AddCommMonoid T]
-                 (N: ℕ)
-                 (S: Fin N → Fin N → T):
-finsum (fun i: Fin N => finsum (fun j: Fin N => if(i=j) then S j i else 0)) =
-finsum (fun i: Fin N => S i i)
-
-private axiom ax6(T: Type)
-                 [AddCommMonoid T]
-                 [Module ℂ T]
-                 (N: ℕ)
-                 (bas: Basis (Fin N) ℂ T):
-∀x: T, x = finsum (fun i: Fin N => (Basis.repr bas x i) • (bas i))
-
 -- check that conjugate is really conjugate
 private theorem test3{T: Type}
              [AddCommMonoid T]
              [Module ℂ T]
              [IP T]
              [ob: OrthonormalBasis T]
+             (pos: ob.N > 0)
              (A: T →ₗ[ℂ] T): isAdj A (adj A) := by
   simp [isAdj, adj, operCompAdj, OP]
   intro x y
-  simp [ax1]
-  rw [ax2 T ob.N (IP.f) IP.distrLeft IP.distrRight (fun i:Fin ob.N => IP.f (A (OrthonormalBasis.basis i)) x • OrthonormalBasis.basis i) y]
+  simp [FS.applyMap]
+  rw [FS.distrLeft T ob.N (IP.f) IP.distrLeft (by sorry) (fun i:Fin ob.N => IP.f (A (OrthonormalBasis.basis i)) x • OrthonormalBasis.basis i) y]
   simp [IP.smulLeft, IP.smulRight]
   simp [IP.comm _ x]
   have eq: ∀i: Fin ob.N,
@@ -130,13 +82,13 @@ private theorem test3{T: Type}
     ring_nf
     aesop
   simp [eq]
-  let pr1 := ax3 T ob.N (IP.f) IP.distrLeft IP.distrRight (fun i:Fin ob.N => ((OP (A (OrthonormalBasis.basis i)) (OrthonormalBasis.basis i)) y)) x
+  let pr1 := FS.distrRight T ob.N (IP.f) IP.distrRight (by sorry) (fun i:Fin ob.N => ((OP (A (OrthonormalBasis.basis i)) (OrthonormalBasis.basis i)) y)) x
   simp [Eq.symm pr1]
   clear pr1 eq
-  have eq: finsum (fun i : Fin ob.N => (OP (A (OrthonormalBasis.basis i)) (OrthonormalBasis.basis i)) y) = A y := by
-    rw [ax6 T ob.N ob.basis y]
-    simp [ax4]
+  have eq: FS.fs (fun i : Fin ob.N => (OP (A (OrthonormalBasis.basis i)) (OrthonormalBasis.basis i)) y) = A y := by
+    rw [FS.basis T ob.N pos ob.basis y]
+    simp [FS.applyDistr]
     simp [OP]
     simp [ob.prop]
-    simp [ax5]
+    simp [FS.doubleFS T ob.N pos]
   simp [eq]
