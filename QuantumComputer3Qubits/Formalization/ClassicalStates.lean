a8fe6db57199881971aab8dd33c953c2d608a594
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-07-13 09:26:42 +0200

    refactoring

--- QuantumComputer3Qubits/Formalization/ClassicalStates.lean
+++ QuantumComputer3Qubits/Formalization/ClassicalStates.lean
@@ -1,98 +1,71 @@
 import QuantumComputer3Qubits.Formalization.RegistryState
 
--- SE means "State Examples"
-namespace SE
+-- CS means "Classical States"
+namespace CS
+
+@[reducible]
+-- CV means "classical value"
+def CV: Type := Fin 2
 
 @[simp]
-def s0: StateReg1 :=
-fun x:Fin 2 => match x with
-| 0 => 1
-| 1 => 0
-@[simp]
-def s1: StateReg1 :=
-fun x:Fin 2 => match x with
-| 0 => 0
-| 1 => 1
+-- q means "qubit"
+def q(val: CV): StateReg1 :=
+match val with
+-- analog of state of classical bit with value 0
+| 0 => fun x:Fin 2 => match x with
+                      | 0 => 1
+                      | 1 => 0
+-- analog of state of classical bit with value 0
+| 1 => fun x:Fin 2 => match x with
+                      | 0 => 0
+                      | 1 => 1
 
-@[simp]
-noncomputable
-def s00: StateReg2 := TensorProduct.tmul ℂ s0 s0
-@[simp]
-noncomputable
-def s01: StateReg2 := TensorProduct.tmul ℂ s0 s1
-@[simp]
-noncomputable
-def s10: StateReg2 := TensorProduct.tmul ℂ s1 s0
-@[simp]
-noncomputable
-def s11: StateReg2 := TensorProduct.tmul ℂ s1 s1
-
-def si0(i: Fin 3): StateReg1Ind i :=
-match i with
-| 0 => fun x: X1 => match x with
-                    | X1.a => 1
-                    | X1.b => 0
-| 1 => fun x: X2 => match x with
-                    | X2.a => 1
-                    | X2.b => 0
-| 2 => fun x: X3 => match x with
-                    | X3.a => 1
-                    | X3.b => 0
-
-def si1(i: Fin 3): StateReg1Ind i :=
-match i with
-| 0 => fun x: X1 => match x with
-                    | X1.a => 0
-                    | X1.b => 1
-| 1 => fun x: X2 => match x with
-                    | X2.a => 0
-                    | X2.b => 1
-| 2 => fun x: X3 => match x with
-                    | X3.a => 0
-                    | X3.b => 1
+lemma test1: (q 0 0 = 1) ∧
+             (q 0 1 = 0) ∧
+             (q 1 0 = 0) ∧
+             (q 1 1 = 1) := by
+  simp
 
 @[simp]
 noncomputable
-def si00(i₁ i₂: Fin 3)(ord: i₁ < i₂):
-  StateReg2Ind i₁ i₂ ord :=
-  TensorProduct.tmul ℂ (si0 i₁) (si0 i₂)
-@[simp]
-noncomputable
-def si01(i₁ i₂: Fin 3)(ord: i₁ < i₂):
-  StateReg2Ind i₁ i₂ ord :=
-  TensorProduct.tmul ℂ (si0 i₁) (si1 i₂)
-@[simp]
-noncomputable
-def si10(i₁ i₂: Fin 3)(ord: i₁ < i₂):
-  StateReg2Ind i₁ i₂ ord :=
-  TensorProduct.tmul ℂ (si1 i₁) (si0 i₂)
-@[simp]
-noncomputable
-def si11(i₁ i₂: Fin 3)(ord: i₁ < i₂):
-  StateReg2Ind i₁ i₂ ord :=
-  TensorProduct.tmul ℂ (si1 i₁) (si1 i₂)
+-- qq means 2 qubits
+def qq(val1 val2: CV): StateReg2 := TensorProduct.tmul ℂ (q val1) (q val2)
+
+-- qi means "qubit indexed"
+def qi(val: CV)(i: Fin 3): StateReg1Ind i :=
+match val with
+-- analog of state of classical bit with value 0
+| 0 => match i with
+       | 0 => fun x: X1 => match x with
+                          | X1.a => 1
+                          | X1.b => 0
+       | 1 => fun x: X2 => match x with
+                          | X2.a => 1
+                          | X2.b => 0
+       | 2 => fun x: X3 => match x with
+                          | X3.a => 1
+                          | X3.b => 0
+-- analog of state of classical bit with value 1
+| 1 => match i with
+       | 0 => fun x: X1 => match x with
+                          | X1.a => 0
+                          | X1.b => 1
+       | 1 => fun x: X2 => match x with
+                          | X2.a => 0
+                          | X2.b => 1
+       | 2 => fun x: X3 => match x with
+                          | X3.a => 0
+                          | X3.b => 1
 
 @[simp]
 noncomputable
-def s000: StateReg3 := TensorProduct.tmul ℂ (si00 0 1 (by aesop)) (si0 2)
+def qqi(val1 val2: CV)(i1 i2: Fin 3)(ord: i1 < i2):
+  StateReg2Ind i1 i2 ord :=
+  TensorProduct.tmul ℂ (qi val1 i1) (qi val2 i2)
+
 @[simp]
 noncomputable
-def s001: StateReg3 := TensorProduct.tmul ℂ (si00 0 1 (by aesop)) (si1 2)
-@[simp]
-noncomputable
-def s010: StateReg3 := TensorProduct.tmul ℂ (si01 0 1 (by aesop)) (si0 2)
-@[simp]
-noncomputable
-def s011: StateReg3 := TensorProduct.tmul ℂ (si01 0 1 (by aesop)) (si1 2)
-@[simp]
-noncomputable
-def s100: StateReg3 := TensorProduct.tmul ℂ (si10 0 1 (by aesop)) (si0 2)
-@[simp]
-noncomputable
-def s101: StateReg3 := TensorProduct.tmul ℂ (si10 0 1 (by aesop)) (si1 2)
-@[simp]
-noncomputable
-def s110: StateReg3 := TensorProduct.tmul ℂ (si11 0 1 (by aesop)) (si0 2)
-@[simp]
-noncomputable
-def s111: StateReg3 := TensorProduct.tmul ℂ (si11 0 1 (by aesop)) (si1 2)
+def s(val1 val2 val3: CV): StateReg3 :=
+TensorProduct.tmul ℂ
+                   (qqi val1 val2 0 1 (by aesop))
+                   (qi val3 2)
