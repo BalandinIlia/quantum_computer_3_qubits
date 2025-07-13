72d78cd1879bea26d8fca71b09d783cc68387ee0
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-07-13 12:19:24 +0200

    fix

--- QuantumComputer3Qubits/Tests/TransformOperators.lean
+++ QuantumComputer3Qubits/Tests/TransformOperators.lean
@@ -23,7 +23,7 @@ def not:OP.o1:=
   map_smul' := by aesop
 }
 
-theorem notProp: (not SE.s0 = SE.s1) ∧ (not SE.s1 = SE.s0) := by
+theorem notProp: (not (CS.q 0) = (CS.q 1)) ∧ (not (CS.q 1) = (CS.q 0)) := by
   simp [not]
   aesop
 
@@ -33,8 +33,8 @@ theorem test1:∀i: Fin 3, ∀s: StateReg1Ind i,
   aesop
 
 theorem test2:∀i: Fin 3,
-              (TO.o1_oi1 i not (SE.si0 i) = (SE.si1 i)) ∧
-              (TO.o1_oi1 i not (SE.si1 i) = (SE.si0 i)) := by
+              (TO.o1_oi1 i not (CS.qi 0 i) = (CS.qi 1 i)) ∧
+              (TO.o1_oi1 i not (CS.qi 1 i) = (CS.qi 0 i)) := by
   simp [TO.o1_oi1, LER.reg1i_reg1]
   aesop
 
@@ -44,20 +44,20 @@ noncomputable
 def notInd(i: Fin 3):OP.oi1 i := TO.o1_oi1 i not
 
 theorem test3:
-(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) SE.s000 = SE.s101) ∧
-(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) SE.s010 = SE.s111) ∧
-(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) SE.s101 = SE.s000) ∧
-(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) SE.s111 = SE.s010) ∧
+(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) CS.s000 = CS.s101) ∧
+(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) CS.s010 = CS.s111) ∧
+(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) CS.s101 = CS.s000) ∧
+(TO.tpo2o1i 0 1 (by aesop) 2 (by aesop) (by aesop) (notIdInd 0 1 (by aesop)) (notInd 2) CS.s111 = CS.s010) ∧
 
-(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) SE.s000 = SE.s110) ∧
-(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) SE.s010 = SE.s100) ∧
-(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) SE.s011 = SE.s101) ∧
-(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) SE.s111 = SE.s001) ∧
+(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) CS.s000 = CS.s110) ∧
+(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) CS.s010 = CS.s100) ∧
+(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) CS.s011 = CS.s101) ∧
+(TO.tpo2o1i 0 2 (by aesop) 1 (by aesop) (by aesop) (notIdInd 0 2 (by aesop)) (notInd 1) CS.s111 = CS.s001) ∧
 
-(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) SE.s000 = SE.s110) ∧
-(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) SE.s010 = SE.s100) ∧
-(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) SE.s011 = SE.s101) ∧
-(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) SE.s111 = SE.s001) := by
+(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) CS.s000 = CS.s110) ∧
+(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) CS.s010 = CS.s100) ∧
+(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) CS.s011 = CS.s101) ∧
+(TO.tpo2o1i 1 2 (by aesop) 0 (by aesop) (by aesop) (notIdInd 1 2 (by aesop)) (notInd 0) CS.s111 = CS.s001) := by
   all_goals try apply And.intro
   all_goals try apply And.intro
   all_goals try apply And.intro
