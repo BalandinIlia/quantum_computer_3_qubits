a6f7a928394ec1dc3cf474f6aba489bc25cf1540
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-07-14 23:53:38 +0200

    operators

--- QuantumComputer3Qubits/Formalization/Operators.lean
+++ QuantumComputer3Qubits/Formalization/Operators.lean
@@ -29,7 +29,7 @@ def oi2(i1 i2: Fin 3)(ord: (i1 < i2)):Type :=
   (StateReg2Ind i1 i2 ord) →ₗ[ℂ] (StateReg2Ind i1 i2 ord)
 
 lemma Equality3(op1 op2: o3):
-(∀ v1 v2 v3: Fin 2, (op1 (CS.qqq v1 v2 v3) = op2 (CS.qqq v1 v2 v3))) →
+(∀ v1 v2 v3: CS.CV, (op1 (CS.qqq v1 v2 v3) = op2 (CS.qqq v1 v2 v3))) →
 op1 = op2 := by
   intro h
   have t:∀s: StateReg3, op1 s = op2 s := by
@@ -39,4 +39,53 @@ op1 = op2 := by
     simp [st_x]
     simp [CS.qqq] at h
     simp [h]
+  aesop
+
+def Core: Type := CS.CV → CS.CV → CS.CV → StateReg3
+
+noncomputable
+def o3ByCore(core: Core): o3 := FS.fs
+(fun i: Fin 8 => match i with
+| 0 => OP (core 0 0 0) (CS.qqq 0 0 0)
+| 1 => OP (core 0 0 1) (CS.qqq 0 0 1)
+| 2 => OP (core 0 1 0) (CS.qqq 0 1 0)
+| 3 => OP (core 0 1 1) (CS.qqq 0 1 1)
+| 4 => OP (core 1 0 0) (CS.qqq 1 0 0)
+| 5 => OP (core 1 0 1) (CS.qqq 1 0 1)
+| 6 => OP (core 1 1 0) (CS.qqq 1 1 0)
+| 7 => OP (core 1 1 1) (CS.qqq 1 1 1)
+)
+
+noncomputable
+def o3AdjByCore(core: Core): o3 := FS.fs
+(fun i: Fin 8 => match i with
+| 0 => OP (CS.qqq 0 0 0) (core 0 0 0)
+| 1 => OP (CS.qqq 0 0 1) (core 0 0 1)
+| 2 => OP (CS.qqq 0 1 0) (core 0 1 0)
+| 3 => OP (CS.qqq 0 1 1) (core 0 1 1)
+| 4 => OP (CS.qqq 1 0 0) (core 1 0 0)
+| 5 => OP (CS.qqq 1 0 1) (core 1 0 1)
+| 6 => OP (CS.qqq 1 1 0) (core 1 1 0)
+| 7 => OP (CS.qqq 1 1 1) (core 1 1 1)
+)
+
+theorem o3CoreAdj: ∀c: Core, HC.adj (o3ByCore c) = o3AdjByCore c := by
+  intro c
+  let ⟨A,prEx,prEq⟩ := HC.adjEx! (o3ByCore c)
+  simp at prEx prEq
+  have eq1: HC.adj (o3ByCore c) = A := by
+    apply prEq (HC.adj (o3ByCore c))
+    apply HC.reallyAdj
+  have eq2: o3AdjByCore c = A := by
+    apply prEq (o3AdjByCore c)
+    simp [o3AdjByCore, o3ByCore, FS.FS8]
+    apply HC.adjSum
+    all_goals try apply HC.adjSum
+    all_goals try apply HC.adjSum
+    all_goals try apply HC.adjSum
+    all_goals try apply HC.adjSum
+    all_goals try apply HC.adjSum
+    all_goals try apply HC.adjSum
+    all_goals try apply HC.adjOP
+  clear prEx prEq
   aesop
